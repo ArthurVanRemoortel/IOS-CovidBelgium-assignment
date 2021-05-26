69dde3a62a14c461f8c31c1a9e1a32dd3894d813
ArthurVanRemoortel <arthurvanremoortel@gmail.com>
2021-05-26 15:32:28 +0200

    Added .gitignore

---
+++ EhB-IOS-Werkstuk2/EhB-IOS-Werkstuk2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EhB-IOS-Werkstuk2
+//
+//  Created by Arthur Van Remoortel on 26/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
