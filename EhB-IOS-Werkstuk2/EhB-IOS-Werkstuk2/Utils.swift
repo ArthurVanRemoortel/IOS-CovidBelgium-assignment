9f4fac43d6f8caf75feaf76ee19fff2e529e8f0d
ArthurVanRemoortel <arthurvanremoortel@gmail.com>
2021-05-27 18:51:12 +0200

    Added colors

---
+++ EhB-IOS-Werkstuk2/EhB-IOS-Werkstuk2/Utils.swift
@@ -0,0 +1,45 @@
+//
+//  Utils.swift
+//  EhB-IOS-Werkstuk2
+//
+//  Created by Arthur Van Remoortel on 27/05/2021.
+//
+
+import Foundation
+import Charts
+
+
+struct GreenColor {
+    let uiColor = UIColor(red: 0.199, green: 0.765, blue: 0.309, alpha: 1.00)
+    let hexString = "32c34e"
+}
+
+struct DarkestBlueColor {
+    let uiColor = UIColor(red: 0.133, green: 0.133, blue: 0.231, alpha: 1.00)
+    let hexString = "22223b"
+}
+
+struct DarkBlueColor {
+    let uiColor = UIColor(red: 0.290, green: 0.306, blue: 0.412, alpha: 1.00)
+    let hexString = "4a4e69"
+}
+
+
+struct PurpleColor {
+    let uiColor = UIColor(red: 0.604, green: 0.549, blue: 0.596, alpha: 1.00)
+    let hexString = "9a8c98"
+}
+
+
+struct PinkColor {
+    let uiColor = UIColor(red: 0.788, green: 0.678, blue: 0.655, alpha: 1.00)
+    let hexString = "c9ada7"
+}
+
+struct LightPinkColor {
+    let uiColor = UIColor(red: 0.949, green: 0.914, blue: 0.894, alpha: 1.00)
+    let hexString = "f2e9e4"
+}
+
+struct AppColors {
+}
