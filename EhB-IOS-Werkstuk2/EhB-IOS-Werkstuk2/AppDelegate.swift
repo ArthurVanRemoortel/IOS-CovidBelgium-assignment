1fe1e7e496cbf558a72adb5b8278024e9b5681a6
ArthurVanRemoortel <arthurvanremoortel@gmail.com>
2021-05-27 16:25:21 +0200

    Partial functional API download and storage

--- EhB-IOS-Werkstuk2/EhB-IOS-Werkstuk2/AppDelegate.swift
+++ EhB-IOS-Werkstuk2/EhB-IOS-Werkstuk2/AppDelegate.swift
@@ -11,57 +11,34 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        self.resetFirstLaunch() // TODO: Development only. Remove this.
+        self.startupCheck()
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
     // MARK: - Core Data stack
 
     lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-        */
         let container = NSPersistentContainer(name: "EhB_IOS_Werkstuk2")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
         return container
     }()
-
-    // MARK: - Core Data Saving support
 
     func saveContext () {
         let context = persistentContainer.viewContext
@@ -74,6 +51,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 let nserror = error as NSError
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
+        }
+    }
+    
+    func startupCheck(){
+        // http://ios-tutorial.com/detect-application-run-first-time-ios/
+        let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
+        if launchedBefore {
+            print("Not first launch.")
+        }
+        else {
+            print("First launch")
+            let context = self.persistentContainer.viewContext
+            UserDefaults.standard.set(true, forKey: "launchedBefore")
+            
+            //TODO: Wrap this is single function?
+            CovidDataManager.sharedInstance.getVaccinations(context: context)
+            CovidDataManager.sharedInstance.getCases(context: context)
+            CovidDataManager.sharedInstance.getTests(context: context)
+        }
+    }
+    
+    func resetFirstLaunch() {
+        // TODO: Development only. Remove this.
+        UserDefaults.standard.set(false, forKey: "launchedBefore")
+        
+        let context = persistentContainer.viewContext
+        do {
+            for toDelEntity in ["Vaccination", "Case", "Test"] {
+                let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: toDelEntity)
+                try context.execute(NSBatchDeleteRequest(fetchRequest: fetchRequest))
+                print("Deleted all \(toDelEntity)")
+            }
+        } catch {
+            print("Could to clear database.")
         }
     }
 
--- EhB-IOS-Werkstuk2/EhB-IOS-Werkstuk2/CovidData.swift
+++ EhB-IOS-Werkstuk2/EhB-IOS-Werkstuk2/CovidData.swift
@@ -6,3 +6,112 @@
 //
 
 import Foundation
+import CoreData
+
+
+class CovidDataManager {
+    let dateFormatter = DateFormatter()
+
+    static let sharedInstance: CovidDataManager = {
+        let instance = CovidDataManager()
+        instance.dateFormatter.dateFormat = "yyyy-MM-dd"
+        return instance
+    }()
+    
+    
+    func getCases(context: NSManagedObjectContext){
+        let url = URL(string: "https://epistat.sciensano.be/Data/COVID19BE_CASES_AGESEX.json")!
+        makeGetCall(url: url, completion:  {(data : [[String: AnyObject]]) -> () in
+            for caseData in data{
+                if (caseData.keys.count == 6){ // API sometimes returns incomplete data. Ignore it.
+                    let covidCase = Case(context: context)
+                    covidCase.date = self.dateFormatter.date(from: caseData["DATE"] as! String)!
+                    covidCase.province = caseData["PROVINCE"] as? String
+                    covidCase.region = caseData["REGION"] as? String
+                    covidCase.agegroup = caseData["AGEGROUP"] as? String
+                    covidCase.sex = caseData["SEX"] as? String
+                    covidCase.cases = (caseData["CASES"]?.int16Value)!
+                }
+            }
+            do {
+                try context.save()
+            } catch {
+            }
+        })
+    }
+    
+    func getTests(context: NSManagedObjectContext){
+        let url = URL(string: "https://epistat.sciensano.be/Data/COVID19BE_tests.json")!
+        makeGetCall(url: url, completion:  {(data : [[String: AnyObject]]) -> () in
+            for testData in data{ 
+                if (testData.keys.count == 5){ // API sometimes returns incomplete data. Ignore it.
+                    let test = Test(context: context)
+                    test.date = self.dateFormatter.date(from: testData["DATE"] as! String)!
+                    test.province = testData["PROVINCE"] as? String
+                    test.region = testData["REGION"] as? String
+                    test.test_all = (testData["TESTS_ALL"]?.int16Value)!
+                    test.test_all_pos = (testData["TESTS_ALL_POS"]?.int16Value)!
+                }
+            }
+            do {
+                try context.save()
+            } catch {
+            }
+        })
+    }
+    
+    
+    func getVaccinations(context: NSManagedObjectContext){
+        let url = URL(string: "https://epistat.sciensano.be/Data/COVID19BE_VACC.json")!
+        makeGetCall(url: url, completion:  {(data : [[String: AnyObject]]) -> () in
+            for vaccData in data{
+                if (vaccData.keys.count == 8){ // API sometimes returns incomplete data. Ignore it.
+                    let vacc = Vaccination(context: context)
+                    vacc.date = self.dateFormatter.date(from: vaccData["DATE"] as! String)!
+                    vacc.region = vaccData["REGION"] as? String
+                    vacc.agegroup = vaccData["AGEGROUP"] as? String
+                    vacc.sex = vaccData["SEX"] as? String
+                    vacc.brand = vaccData["BRAND"] as? String
+                    vacc.dose = vaccData["DOSE"] as? String
+                    vacc.agegroup = vaccData["AGEGROUP"] as? String
+                    vacc.count = (vaccData["COUNT"]?.int16Value)!
+                }
+            }
+            do {
+                try context.save()
+            } catch {
+            }
+        })
+    }
+    
+
+    func makeGetCall(url: URL, completion: @escaping ([[String: AnyObject]])->()){
+        let urlRequest = URLRequest(url: url)
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        let task = session.dataTask(with: urlRequest) {
+            (data, response, error) in
+            guard error == nil else {
+                print("error calling GET")
+                print(error!)
+                return
+            }
+            guard let responseData = data else {
+                print("Error: did not receive data")
+                return
+            }
+            do {
+                guard let parsedData = try JSONSerialization.jsonObject(with: responseData, options: []) as? [[String: AnyObject]] else {
+                    print("error trying to convert data to JSON")
+                    return
+                }
+                DispatchQueue.main.async {
+                    completion(parsedData)
+                }
+            } catch  {
+                print("error trying to convert data to JSON")
+                return
+            }
+        }
+        task.resume()
+    }
+    }
