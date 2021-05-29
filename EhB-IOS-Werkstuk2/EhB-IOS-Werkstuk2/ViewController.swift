f8c6a88bdad335b5fa73366d318acd34425cf373
ArthurVanRemoortel <arthurvanremoortel@gmail.com>
2021-05-29 12:32:08 +0200

    Made improvements to UI

--- EhB-IOS-Werkstuk2/EhB-IOS-Werkstuk2/ViewController.swift
+++ EhB-IOS-Werkstuk2/EhB-IOS-Werkstuk2/ViewController.swift
@@ -9,12 +9,19 @@ import UIKit
 import Charts
 import FSInteractiveMap
 
-class ViewController: UIViewController {
-
+class ViewController: UITabBarController {
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if (appDelegate.isFirstLauch || true){ // TODO: Dev Only
+            let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let newViewController = storyBoard.instantiateViewController(withIdentifier: "ImportDataPopupViewController") as! ImportDataPopupViewController
+            self.present(newViewController, animated: true, completion: nil)
+        }
+    }
 }
 
