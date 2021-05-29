a8fe6b967f4f7624dd7e7165856cb1d1a4259f5c
ArthurVanRemoortel <arthurvanremoortel@gmail.com>
2021-05-29 14:18:53 +0200

    Popup asking the user the download the data

--- EhB-IOS-Werkstuk2/EhB-IOS-Werkstuk2/ImportDataPopupViewController.swift
+++ EhB-IOS-Werkstuk2/EhB-IOS-Werkstuk2/ImportDataPopupViewController.swift
@@ -6,16 +6,50 @@
 //
 
 import UIKit
+import CoreData
 
 class ImportDataPopupViewController: UIViewController {
 
+    @IBOutlet var continueButton: UIButton!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    var completedDownloads: Int = 0
+    
+    let persistentContainer = (UIApplication.shared.delegate as! AppDelegate).persistentContainer
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.isModalInPresentation = true
         // Do any additional setup after loading the view.
     }
     
-
+    override func viewDidAppear(_ animated: Bool) {
+        downloadData()
+    }
+    
+    func checkDownloadCompleted(){
+        completedDownloads += 1
+        if (completedDownloads == CovidDataManager.shared.apiUrls.count){
+            // All downlaods completed.
+            DispatchQueue.main.async {
+                self.continueButton.isHidden = false
+            }
+        }
+    }
+    
+    func downloadData(){
+        CovidDataManager.shared.getAllData(context: persistentContainer.viewContext, mainCompletionHandler: checkDownloadCompleted)
+    }
+    
+    @IBAction func openSciensanoWebsite() {
+        if let url = URL(string: "https://www.sciensano.be/en") {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    @IBAction func dismissPopup() {
+        self.dismiss(animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
